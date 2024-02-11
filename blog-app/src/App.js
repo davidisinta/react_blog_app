474a73e9f65ec629ad16a70eccfdcd41f2f34142
David Isinta <david.isinta@gmail.com>
2024-02-11 02:29:39 -0500

    first commit

---
+++ blog-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> My awesome Blog!!!</h1>
+
+      <div id="page-body"> Welcome to my Blog! </div>
+    </div>
+  );
+}
+
+export default App;
