4eb8ca4202812f9d34cf9ab038ad12b0ede1d415
David Isinta <david.isinta@gmail.com>
2024-02-11 03:08:08 -0500

    boiler plate for blog app in place

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
+
+    <BrowserRouter> 
     <div className="App">
+      
       <h1> My awesome Blog!!!</h1>
 
-      <div id="page-body"> Welcome to my Blog! </div>
+      <div id="page-body">
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles" element={<ArticlesList />} />
+          <Route path="/articles/:articleId" element={<ArticlePage />} />
+      
+
+        </Routes>
+
+        
+      </div>
     </div>
+
+    </BrowserRouter>
+
+    
   );
 }
 
---
+++ blog-app/src/pages/ArticlesListPage.js
@@ -0,0 +1,8 @@
+const ArticlesListPage = () => 
+{
+    return (
+        <h1> This is the ArticlesList Page</h1>
+    )
+}
+
+export default ArticlesListPage;
